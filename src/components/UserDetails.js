2036c176d9d681241937509ad320b826b2d60721
rohitdarunkar <rohitdarunkarwork@gmail.com>
2026-04-28 22:14:32 +0530

    submit

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -4,16 +4,20 @@ import { useParams } from "react-router-dom";
 function UserDetails() {
   const { id } = useParams();
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setUser(null); // important for loading state
+    setLoading(true);
 
     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => res.json())
-      .then(data => setUser(data));
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
   }, [id]);
 
-  if (!user) {
+  if (loading) {
     return <div>Loading...</div>;
   }
 
